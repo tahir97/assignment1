720fbe870e7c723b042c84f7414876e31ff26d59
SENSYS\tahir.idrees <tahir.idrees@sensys.com>
2016-12-23 14:53:03 +0500

    code added for app.js and index.html

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -6,7 +6,32 @@ angular.module('LunchCheck', [])
 
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
+  $scope.itemsList = "";
+  $scope.itemsCount = "";
+  $scope.message = "";
 
+  $scope.checkLunchItems = function () {
+     $scope.itemsCount = getItemsCount($scope.itemsList);
+     $scope.message = getMessage ($scope.itemsCount);
+   };
 }
+
+function getItemsCount (itemsList) {
+  if(itemsList == ""){
+    return 0;
+  } else {
+    var itemsArray = itemsList.split(',');
+    return itemsArray.length;
+  }
+}
+function getMessage (itemsCount) {
+   if(itemsCount == 0){
+     return "Please enter data first";
+   } else if(itemsCount <= 3) {
+     return "Enjoy!";
+   } else {
+     return "Too much!";
+   }
+ }
 
 })();
