dd912fe5fe4d3b37582887851e0c7bfb28d1f3bc
SENSYS\tahir.idrees <tahir.idrees@sensys.com>
2016-12-23 12:58:03 +0500

    app.js and angular.min.js files added

---
+++ module1-solution/app.js
@@ -0,0 +1,12 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+
+}
+
+})();
